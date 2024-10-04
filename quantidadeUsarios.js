99a9339e2909e70cd3f9212e2879a216d961c55e
Mariane Jankoski <110405094+mariane12022007@users.noreply.github.com>
2024-10-04 10:37:48 -0300

    Add files via upload

---
+++ quantidadeUsarios.js
@@ -0,0 +1,26 @@
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys({"Facebook": 3049, "Youtube": 2491, "WhatsApp": 2000, "Instagram": 2000, "TikTok": 1562})
+    const quantidadeUsuarios = Object.values(dados)
+  
+  const data = [
+    {
+      x: nomeDasRedes,
+      y: quantidadeUsuarios,
+      type: 'bar',
+      marker: {
+        color: getComputedStyle(document.body).getPropertyValue('--primary-color')
+      }
+    }
+  ]
+  
+  const grafico = document.createElement('div')
+  grafico.className = 'grafico'
+  document.getElementById('graficos-container').appendChild(grafico)
+  Plotly.newPlot(grafico, data)
+  
+  }
+  
+  quantidadeUsuarios()
